d119ae5b9ade5d497d7b12e929e0ff88e7a58435
Pierre-Dureau <dureaupierre53@gmail.com>
2023-08-10 01:05:00 +0200

    this one works everytime

---
+++ lockboxes/0-lockboxes_real.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Determines if all the boxes can be opened"""
+
+
+def unlock(boxes, box_opened, tab):
+    """Unlock boxes recursively"""
+    for n in box_opened:
+        if (n >= len(tab) or tab[n] is True):
+            continue
+        tab[n] = True
+        unlock(boxes, boxes[n], tab)
+
+
+def canUnlockAll(boxes):
+    """Unlock the first box and check the result"""
+    if (len(boxes) == 0):
+        return True
+
+    tab = [False] * len(boxes)
+    tab[0] = True
+
+    unlock(boxes, boxes[0], tab)
+
+    for check in tab:
+        if check is False:
+            return False
+
+    return True
