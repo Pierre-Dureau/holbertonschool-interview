23dcba64fffee7d8c49d078dd1e6684f905a758a
Dureau <dureaupierre53@gmail.com>
2023-08-23 09:37:22 +0200

    Insert in sorted linked list

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * @brief
+ *
+ * @param head
+ * @param number
+ * @return listint_t*
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+    listint_t *current;
+
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = number;
+    new->next = NULL;
+
+    if (*head == NULL)
+        *head = new;
+    else
+    {
+        while (current->next != NULL && current->next->n > new->n)
+            current = current->next;
+		new->next = current->next;
+        current->next = new;
+    }
+
+    return (new);
+}
