15eb4b8dd7deecec82c5eaa9a6ec8657c58a2b42
Pierre Dureau <dureaupierre53@gmail.com>
2023-08-23 12:00:49 +0200

    Insert in sorted linked list

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -27,7 +27,7 @@ listint_t *insert_node(listint_t **head, int number)
         *head = new;
     else
     {
-        while (current->next != NULL && current->next->n > new->n)
+        while (current->next != NULL && current->next->n < new->n)
             current = current->next;
 		new->next = current->next;
         current->next = new;
