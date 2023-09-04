c35c252c86ba3868ce618f01a0baa82d76cd68da
Dureau <dureaupierre53@gmail.com>
2023-09-04 13:13:29 +0200

    minimum operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Method that calculates the fewest number of operations needed
+"""
+
+def minOperations(n):
+    if (n < 1):
+        return 0
+
+    letters = 1
+    copy = 0
+    nbOpe = 0
+
+    while (letters != n):
+        if (n % letters == 0):
+            copy = letters
+            nbOpe += 1
+        letters += copy
+        nbOpe += 1
+
+    return nbOpe
