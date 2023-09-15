ef4ed5363dba036ba35732553d23166d03d7b489
Pierre-Dureau <dureaupierre53@gmail.com>
2023-09-15 09:03:05 +0200

    add all files

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -34,10 +34,10 @@ static bool is_stable(int grid[3][3])
 		for (j = 0; j < 3; j++)
 		{
 			if (grid[i][j] > 3)
-				return false;
+				return (false);
 		}
 
-	return true;
+	return (true);
 }
 
 /**
