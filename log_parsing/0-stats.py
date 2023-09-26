c065c70f2231a94fb054b53ce2570538a174b21c
Dureau <dureaupierre53@gmail.com>
2023-09-26 10:31:45 +0200

    add file

---
+++ log_parsing/0-stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Reads stdin line by line and computes metrics
+"""
+
+import sys
+import signal
+
+status_code = {
+  "200": 0,
+  "301": 0,
+  "400": 0,
+  "401": 0,
+  "403": 0,
+  "404": 0,
+  "405": 0,
+  "500": 0
+}
+count = 0
+size = 0
+
+def print_stats():
+    print(f"File size: {size}")
+    for k,v in status_code.items():
+        if (v != 0):
+            print(f"{k}: {v}")
+
+def signal_handler(sig, frame):
+    print_stats()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+for line in sys.stdin:
+    line_split = line.split()
+    if (len(line_split) != 9):
+        continue
+    count += 1
+    size += int(line_split[8])
+    status_code[line_split[7]] += 1
+
+    if (count % 10 == 0):
+        print_stats()
