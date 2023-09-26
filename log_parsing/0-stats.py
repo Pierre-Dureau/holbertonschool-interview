fbe6f4163e32783d3d7a5b5cefc81727af3bfd30
Dureau <dureaupierre53@gmail.com>
2023-09-26 10:46:27 +0200

    add file

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -4,43 +4,44 @@ import signal
 import sys
 
 
-status_code = {
-  "200": 0,
-  "301": 0,
-  "400": 0,
-  "401": 0,
-  "403": 0,
-  "404": 0,
-  "405": 0,
-  "500": 0
+stats = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
 }
 count = 0
-size = 0
-
-
-def print_stats():
-    """Print the stats each 10 lines"""
-    print(f"File size: {size}")
-    for k,v in status_code.items():
-        if (v != 0):
-            print(f"{k}: {v}")
+file_size = 0
 
 
 def signal_handler(sig, frame):
-    """Handle Ctrl + C"""
-    print_stats()
-    sys.exit(0)
+    """Handle when CTRL + C is pressed"""
+    print_infos()
+
+
+def print_infos():
+    """Prints the stats obtained from the file"""
+    print("File size: {}".format(file_size))
+    for key, value in sorted(stats.items()):
+        if value != 0:
+            print("{:s}: {:d}".format(key, value))
 
 
 signal.signal(signal.SIGINT, signal_handler)
-
 for line in sys.stdin:
-    line_split = line.split()
-    if (len(line_split) != 9):
-        continue
     count += 1
-    size += int(line_split[8])
-    status_code[line_split[7]] += 1
-
-    if (count % 10 == 0):
-        print_stats()
+    line_parsed = line.split()
+    length_line = len(line_parsed)
+    if length_line < 2:
+        continue
+    file_size += int(line_parsed[length_line - 1])
+    if line_parsed[length_line - 2] not in stats.keys():
+        continue
+    stats[line_parsed[length_line - 2]] += 1
+    if count % 10 == 0:
+        print_infos()
+print_infos()
