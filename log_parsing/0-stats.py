0dd19b15721db93d5846aa55f11026ba0471b0c1
Dureau <dureaupierre53@gmail.com>
2023-09-26 10:53:16 +0200

    add file

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module that reads stdin line by line and computes metrics"""
+"""
+    Module that reads stdin line by line and computes metrics
+"""
 import sys
 import signal
 
@@ -19,26 +21,38 @@ size = 0
 
 
 def signal_handler(sig, frame):
-    """Handle Ctrl + C"""
+    """
+        Handle Ctrl + C
+    """
     print_stats()
 
 
 def print_stats():
-    """Print the stats each 10 lines"""
+    """
+        Print the stats each 10 lines
+    """
     print(f"File size: {size}")
     for k,v in status_code.items():
         if (v != 0):
             print(f"{k}: {v}")
 
 
-signal.signal(signal.SIGINT, signal_handler)
-for line in sys.stdin:
-    line_split = line.split()
-    if (len(line_split) != 9):
-        continue
-    count += 1
-    size += int(line_split[8])
-    status_code[line_split[7]] += 1
+def parse_log():
+    """
+        Parse log
+    """
+    signal.signal(signal.SIGINT, signal_handler)
+    for line in sys.stdin:
+        line_split = line.split()
+        if (len(line_split) != 9):
+            continue
+        count += 1
+        size += int(line_split[8])
+        status_code[line_split[7]] += 1
 
-    if (count % 10 == 0):
-        print_stats()
+        if (count % 10 == 0):
+            print_stats()
+
+
+if __name__ == "__main__":
+    parse_log()
