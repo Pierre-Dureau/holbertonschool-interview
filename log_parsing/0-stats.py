be5a2d4f8138d3804c41b49a3dee1af456c8660d
Pierre-Dureau <dureaupierre53@gmail.com>
2023-09-29 19:03:09 +0200

    correct wrong format

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -7,41 +7,44 @@ import signal
 
 
 status_code = {
-	"200": 0,
-	"301": 0,
-	"400": 0,
-	"401": 0,
-	"403": 0,
-	"404": 0,
-	"405": 0,
-	"500": 0
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
 }
 count = 0
 size = 0
 
 
 def signal_handler(sig, frame):
-	"""Handle when CTRL + C is pressed"""
-	print_stats()
+    """Handle when CTRL + C is pressed"""
+    print_stats()
 
 
 def print_stats():
-	"""Print the stats obtained from the file"""
-	print("File size: {}".format(size))
-	for k, v in sorted(status_code.items()):
-		if (v != 0):
-			print("{:s}: {:d}".format(k, v))
+    """Print the stats obtained from the file"""
+    print("File size: {}".format(size))
+    for k, v in sorted(status_code.items()):
+        if (v != 0):
+            print("{:s}: {:d}".format(k, v))
 
 
 signal.signal(signal.SIGINT, signal_handler)
 for line in sys.stdin:
-	line_split = line.split()
-	if (len(line_split) != 9):
-		continue
-	count += 1
-	size += int(line_split[8])
-	status_code[line_split[7]] += 1
+    line_split = line.split()
+    len_line = len(line_split)
+    if (len_line == 1):
+        continue
+    count += 1
+    size += int(line_split[len_line - 1])
+    if (line_split[len_line - 2] not in status_code.keys()):
+        continue
+    status_code[line_split[len_line - 2]] += 1
 
-	if (count % 10 == 0):
-		print_stats()
+    if (count % 10 == 0):
+        print_stats()
 print_stats()
