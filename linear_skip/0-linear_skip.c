bcde239edc749f67feb05f3612aa29cb36dea7ed
Dureau <dureaupierre53@gmail.com>
2023-11-27 10:52:09 +0100

    fix betty

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -1,5 +1,13 @@
 #include "search.h"
 
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers
+ *
+ * @list: List of node
+ * @value: Int
+ * Return: The node
+ */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *save = list, *temp;
@@ -7,19 +15,21 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	if (!list)
 		return (NULL);
 
-	for(list = list->express; list; list = list->express, save = save->express)
+	for (list = list->express; list; list = list->express, save = save->express)
 	{
 		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
-		if (list->n > value)
+		if (list->n >= value)
 			break;
 	}
 
 	if (list)
-		printf("Value found between indexes [%lu] and [%lu]\n", save->index, list->index);
+		printf("Value found between indexes [%lu] and [%lu]\n", save->index,
+				list->index);
 	else
 	{
 		for (temp = save; temp->next; temp = temp->next);
-		printf("Value found between indexes [%lu] and [%lu]\n", save->index, temp->index);
+		printf("Value found between indexes [%lu] and [%lu]\n", save->index,
+				temp->index);
 	}
 
 	for (; save && save != list; save = save->next)
