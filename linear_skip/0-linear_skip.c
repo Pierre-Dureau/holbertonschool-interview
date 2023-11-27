6c8c823e056ac9195ffd9b92b24ec546dc738ed9
Dureau <dureaupierre53@gmail.com>
2023-11-27 10:40:47 +0100

    fix printf

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -9,22 +9,22 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 	for(list = list->express; list; list = list->express, save = save->express)
 	{
-		printf("Value checked at index [%llu] = [%d]\n", list->index, list->n);
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
 		if (list->n > value)
 			break;
 	}
 
 	if (list)
-		printf("Value found between indexes [%llu] and [%llu]\n", save->index, list->index);
+		printf("Value found between indexes [%lu] and [%lu]\n", save->index, list->index);
 	else
 	{
 		for (temp = save; temp->next; temp = temp->next);
-		printf("Value found between indexes [%llu] and [%llu]\n", save->index, temp->index);
+		printf("Value found between indexes [%lu] and [%lu]\n", save->index, temp->index);
 	}
 
 	for (; save && save != list; save = save->next)
 	{
-		printf("Value checked at index [%llu] = [%d]\n", save->index, save->n);
+		printf("Value checked at index [%lu] = [%d]\n", save->index, save->n);
 		if (save->n == value)
 			return (save);
 	}
