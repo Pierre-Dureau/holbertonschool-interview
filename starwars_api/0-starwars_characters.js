2c631854917d77765ad8ab88549285f4ebe24742
Pierre-Dureau <dureaupierre53@gmail.com>
2023-12-04 06:30:05 +0100

    semistandard

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -3,20 +3,33 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-function doRequest(url) {
+function doRequest (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
         resolve(JSON.parse(body));
+      }
     });
   });
 }
 
-async function main() {
+async function main () {
+  try {
     const filmData = await doRequest(url);
+
     for (const characterURL of filmData.characters) {
+      try {
         const characterData = await doRequest(characterURL);
         console.log(characterData.name);
+      } catch (error) {
+        console.error('Erreur:', error);
+      }
     }
+  } catch (error) {
+    console.error('Erreur:', error);
+  }
 }
 
 main();
