e177a4b6228eba46a06da45130b37dfb10cd9878
Pierre-Dureau <dureaupierre53@gmail.com>
2023-12-11 13:24:31 +0100

    fix special case

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" Rain Module """
+"""Rain Module"""
+
 
 def rain(walls):
     """return how many square units of water will be retained after it rains"""
     water = 0
     j = 0
-    space = 0
+    length = len(walls)
 
-    for i in walls:
-        if i == 0:
-            space += 1
-        else:
-            if i < j:
-                water += i * space
-            else:
-                water += j * space
+    if (length < 3):
+        return 0
+
+    for i in range(1, length):
+        if (walls[i] > walls[i - 1]) and (i == length - 1 or walls[i] > walls[i + 1]):
+            height = walls[j] if walls[j] < walls[i] else walls[i]
+            for k in range(j + 1, i):
+                water += height - walls[k]
             j = i
-            space = 0
+
     return water
