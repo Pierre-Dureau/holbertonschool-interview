faef3b9f8c4c9daf7b88d5dffc8ffa3eaa0b7b97
Pierre-Dureau <dureaupierre53@gmail.com>
2024-01-08 09:30:12 +0100

    add files

---
+++ count_it/0-count.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Module 0-count """
+import requests
+
+
+def count_words(subreddit, word_list, count={}, after=None):
+    """ Queries the Reddit API, parses the title of all hot articles,
+    and prints a sorted count of given keywords """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {"User-Agent": "MonScriptPython/1.0"}
+    params = {'after': after} if after else {}
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json().get("data")
+        for post in data.get("children"):
+            for word in post.get('data').get('title').lower().split():
+                for list in word_list:
+                    if list.lower() == word:
+                        if word in count.keys():
+                            count[word] += 1
+                        else:
+                            count[word] = 1
+
+        if 'after' in data and data.get("after"):
+            return count_words(subreddit, word_list, count, data.get("after"))
+
+        count = dict(sorted(count.items(), key=lambda item: item[1], reverse=True))
+        for key, value in count.items():
+            print(f"{key}: {value}")
+    else:
+        return
