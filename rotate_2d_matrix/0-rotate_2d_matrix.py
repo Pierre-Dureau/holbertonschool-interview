c34a0bc37ee853ee27246dbf8a2a8c15cc10f34a
Pierre-Dureau <dureaupierre53@gmail.com>
2024-02-05 13:00:02 +0100

    add files

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module rotate 2D matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotate a 2D matrix at 90°"""
+    matrix_copy = [line.copy() for line in matrix]
+    length = len(matrix) - 1
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            matrix_copy[j][length] = matrix[i][j]
+        length -= 1
+    print(matrix_copy)
