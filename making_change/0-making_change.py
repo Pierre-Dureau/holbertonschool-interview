b9b84801978d369296b4c2973b17526cbf40f240
Pierre-Dureau <dureaupierre53@gmail.com>
2024-02-26 12:33:22 +0100

    add files

---
+++ making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Make Change function
+"""
+
+
+def makeChange(coins, total):
+    """Determine the fewest number of coins needed to meet a given amount"""
+    n = 0
+    
+    if (total <= 0):
+        return 0
+
+    while (total != 0):
+        highest = 0
+        for coin in coins:
+            if (coin > highest and coin < total):
+                highest = coin
+        if (highest == 0):
+            return -1
+        while (highest <= total):
+            total -= highest
+            n += 1
+
+    return n
