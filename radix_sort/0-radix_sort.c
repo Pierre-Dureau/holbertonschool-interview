55b468c192035fb1315970d930301844b1444295
Pierre-Dureau <dureaupierre53@gmail.com>
2024-03-11 13:03:14 +0100

    edit stop condition

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -2,13 +2,38 @@
 
 
 /**
+ * get_max - get maximum number of unit in the array
+ *
+ * @array: The array
+ * @size: Size of the array
+*/
+int get_max(int *array, size_t size)
+{
+	int max = 0, i = 0, nb = 0;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	while (max)
+	{
+		max /= 10;
+		nb++;
+	}
+
+	return (nb);
+}
+
+
+/**
  * copy_array - Copy an array
  *
  * @new_array: The array
  * @array: The array copied
  * @size: Size of the array
  */
-
 void copy_array(int *new_array, int *array, size_t size)
 {
 	int i;
@@ -26,7 +51,7 @@ void copy_array(int *new_array, int *array, size_t size)
 */
 void radix_sort(int *array, size_t size)
 {
-	int *new_array, i = 0, j = 0, k = 0, u = 1, check = 1;
+	int *new_array, i = 0, j = 0, k = 0, u = 1, nb = 0;
 
 	if (size < 2)
 		return;
@@ -35,27 +60,23 @@ void radix_sort(int *array, size_t size)
 	if (!new_array)
 		return;
 	copy_array(new_array, array, size);
+	nb = get_max(array, size);
 
-	for (u = 1; check == 1; u *= 10, k = 0)
+	for (u = 1; nb > 0; u *= 10, k = 0, nb--)
 	{
-		check = 0;
 		for (i = 0; i < 10; i++)
 		{
 			for (j = 0; j < (int)size; j++)
 			{
-				if (array[j] / u > 0 && (array[j] / u) % 10 == i)
+				if ((array[j] / u) % 10 == i)
 				{
 					new_array[k] = array[j];
 					k++;
-					check = 1;
 				}
 			}
 		}
-		if (check == 1)
-		{
-			print_array(new_array, size);
-			copy_array(array, new_array, size);
-		}
-
+		copy_array(array, new_array, size);
+		print_array(array, size);
 	}
+	free (new_array);
 }
