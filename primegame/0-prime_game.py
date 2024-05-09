051ca428522d8685cc45a70a4d10fd29f9e86a18
Pierre-Dureau <dureaupierre53@gmail.com>
2024-05-09 17:38:54 +0200

    add files

---
+++ primegame/0-prime_game.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module of Prime Game"""
+
+
+def isWinner(x, nums):
+    """Return the winner between Maria and Ben"""
+    ben = 0
+    maria = 0
+
+    for i in range(0, x):
+        turn = 0
+        tab = [j for j in range(1, nums[i] + 1)]
+
+        while (len(tab) != 1):
+            choice = tab[1]
+            temp = []
+            for k in tab:
+                if (k % choice == 0):
+                    temp.append(k)
+            tab = temp
+            turn += 1
+
+        if (turn % 2 == 0):
+            ben += 1
+        else:
+            maria += 1
+
+    return "Ben" if ben > maria else "Maria"
